febfc5fc9ecabffae2eff1c994d5b8daf3210a22
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-02-27 00:42:02 +0200

    update

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -4,12 +4,15 @@ import PropTypes from 'prop-types';
 import { Input } from './Filter.styled';
 
 export const Filter = ({ onChange, contacts }) => {
-  const disabled = contacts[0] ? false : true;
   return (
     <Section>
       <Wrapper>
         <label htmlFor="filter">Search contacts by name</label>
-        <Input name="filter" onChange={onChange} disabled={disabled}></Input>
+        <Input
+          name="filter"
+          onChange={onChange}
+          placeholder="type contacts name"
+        ></Input>
       </Wrapper>
     </Section>
   );
